0383e515adee8e421880c670e72e878cfd44d2f5
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2026-03-02 10:07:46 -0500

    v0.1.20
    This is a minor update that fixes a few bugs, improves code readability, and adds menu labels for debugging.

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 
 // go build -ldflags "-X 'main.DEBUG_MODE=false'" .
 var DEBUG_MODE string = "true"
-var FW_VERSION string = "0.1.19 (2.28.2026)"
+var FW_VERSION string = "0.1.20 (3.2.2026)"
 var EXIT_MODE uint8 = 0 // 0 - none, 1 - shutdown, 2 - reboot, 3 - soft restart
 
 const (
